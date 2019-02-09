fc625526da901bdb1f7ca058d032876d4a4f7c31
Neareeves <alethesj@gmail.com>
2019-02-09 14:04:15 +0100

    bdd corrigée

---
+++ BDD/le_potager_de_roger.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Sam 09 Février 2019 à 13:59
+-- Version du serveur :  5.7.25-0ubuntu0.18.04.2
+-- Version de PHP :  7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `le_potager_de_roger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id_categorie` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres`
+--
+
+CREATE TABLE `offres` (
+  `id_offre` int(11) NOT NULL,
+  `url_photo` varchar(5) COLLATE utf8mb4_bin NOT NULL,
+  `commentaire` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `date_creation` date NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres_categories`
+--
+
+CREATE TABLE `offres_categories` (
+  `id_offre` int(11) NOT NULL,
+  `id_categorie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `num_rue` smallint(6) DEFAULT NULL,
+  `adresse` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `code_postal` smallint(6) NOT NULL,
+  `commune` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `statut` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `nb_offres` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_offres`
+--
+
+CREATE TABLE `users_offres` (
+  `id_offre` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_categorie`);
+
+--
+-- Index pour la table `offres`
+--
+ALTER TABLE `offres`
+  ADD PRIMARY KEY (`id_offre`),
+  ADD UNIQUE KEY `offres_AK` (`id_user`);
+
+--
+-- Index pour la table `offres_categories`
+--
+ALTER TABLE `offres_categories`
+  ADD PRIMARY KEY (`id_offre`,`id_categorie`),
+  ADD KEY `offres_categories_categories0_FK` (`id_categorie`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Index pour la table `users_offres`
+--
+ALTER TABLE `users_offres`
+  ADD PRIMARY KEY (`id_offre`,`id_user`),
+  ADD KEY `users_offres_users0_FK` (`id_user`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `offres`
+--
+ALTER TABLE `offres`
+  MODIFY `id_offre` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `offres_categories`
+--
+ALTER TABLE `offres_categories`
+  ADD CONSTRAINT `offres_categories_categories0_FK` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`),
+  ADD CONSTRAINT `offres_categories_offres_FK` FOREIGN KEY (`id_offre`) REFERENCES `offres` (`id_offre`);
+
+--
+-- Contraintes pour la table `users_offres`
+--
+ALTER TABLE `users_offres`
+  ADD CONSTRAINT `users_offres_offres_FK` FOREIGN KEY (`id_offre`) REFERENCES `offres` (`id_offre`),
+  ADD CONSTRAINT `users_offres_users0_FK` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
