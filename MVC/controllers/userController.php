3995da6bbb7c369ee7d360799f8cf8ad6b87345e
Yahadach <ahadachyouness@gmail.com>
2019-02-10 11:42:42 +0100

    [3] youness, ajout de la base de données remplie + ajout de l'icone utilisateur

--- MVC/controllers/userController.php
+++ MVC/controllers/userController.php
@@ -1,7 +1,7 @@
 <?php
 
 function getHome($twig,$pdo){
-        $users = $pdo->query("SELECT users.latitude, users.longitude, users.prenom FROM users")->fetchAll();
+        $users = $pdo->query("SELECT users.latitude, users.longitude, users.prenom, offres.commentaire FROM users, users_offres, offres WHERE users.id_user=users_offres.id_user AND users_offres.id_offre = offres.id_offre")->fetchAll();
         echo $twig->render('base.html.twig', ['users' => $users, 'test' => "bonjour"]);
 }
 
