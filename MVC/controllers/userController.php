86556a5e4177d122c8c02f5c4e04b9c2a17a1508
Yahadach <ahadachyouness@gmail.com>
2019-02-10 09:10:42 +0100

    [2] youness, nettoyage de fichiers du mvc

--- MVC/controllers/userController.php
+++ MVC/controllers/userController.php
@@ -1,7 +1,7 @@
 <?php
 
 function getHome($twig,$pdo){
-        $users = $pdo->query("SELECT users.latitude, users.longitude FROM users")->fetchAll();
+        $users = $pdo->query("SELECT users.latitude, users.longitude, users.prenom FROM users")->fetchAll();
         echo $twig->render('base.html.twig', ['users' => $users, 'test' => "bonjour"]);
 }
 
